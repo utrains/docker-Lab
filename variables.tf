a8c036166602eb9bd3cc9af6de6293f037c1e6c6
Arcdarelle <162192195+Arcdarelle@users.noreply.github.com>
2025-02-25 21:49:12 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,26 @@
+
+
+variable "region" {
+  type = string
+  default = "us-east-1"
+}
+
+variable "VPC_cidr" {
+  type = string
+  default = "192.168.0.0/16" 
+}
+
+variable "public_subnet_cidr" {
+  type = string
+  default = "192.168.1.0/24"
+}  
+
+variable "AZ" {
+  type = string
+  default = "us-east-2a"
+}
+
+variable "project-name" {
+  type    = string
+  default = "docker-lab"
+}
