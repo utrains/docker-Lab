908bcb0400721aa2f9e336bb2bf14d11e302d5a6
Kanaestephe <kanaestephe@gmail.com>
2025-04-18 18:20:15 +0100

    update docker compose to the latest version

--- install.sh
+++ install.sh
@@ -12,6 +12,12 @@ sudo yum install git -y
 # install wget
 sudo yum install wget -y
 
-#install docker compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.20.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+# Install docker compose
+
+# Get the latest version number of Docker Compose from GitHub
+latest_version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep '"tag_name":' | cut -d '"' -f 4)
+
+# Download Docker Compose binary
+sudo curl -L "https://github.com/docker/compose/releases/download/${latest_version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# grant permissions
 sudo chmod +x /usr/local/bin/docker-compose
